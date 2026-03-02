248e19426ee3f03e75a4a9f0ed825dc6128ab4ab
aimar <aimarnegro@gmail.com>
2026-03-02 21:19:33 +0100

    Añadida personalización de logo y opciones en el drawer

--- app/src/main/java/com/das/proyectodas/MainActivity.java
+++ app/src/main/java/com/das/proyectodas/MainActivity.java
@@ -7,6 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,11 +21,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Inicializar DrawerLayout
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawer_layout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //Configurar Fragment
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        if(navHostFragment != null){
+            NavController navController = navHostFragment.getNavController();
+            NavigationView navigationView = findViewById(R.id.nav_view);
+
+            navigationView.setItemIconTintList(null);
+
+            NavigationUI.setupWithNavController(navigationView, navController);
+        }
 
     }
 }
